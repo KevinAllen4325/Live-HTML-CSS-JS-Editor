645da93419fbc13c48301ddb14ef0cb331f3db95
Kevin O'Neill <kevinallenoneill@gmail.com>
2017-04-28 11:13:15 -0500

    Files for editor

---
+++ app.js
@@ -0,0 +1,12 @@
+function compile () {
+  var html = document.getElementById("html");
+  var css = document.getElementById("css");
+  var js = document.getElementById("js");
+  var code = document.getElementById("code").contentWindow.document;
+
+  document.body.onkeyup = function () {
+    code.open();
+    code.writeln(html.value + "<style>" +css.value + "</style>" + "<style>" + js.value + "</style>");
+  };
+};
+compile();
